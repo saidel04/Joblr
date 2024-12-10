7f4872bf0a55f90b98e20343571bfae16973d986
saidel04 <saidelakad@cmail.carleton.ca>
2024-12-10 07:18:07 -0500

    decided to combine the user data to users table and consolidate everything making it easier to find

--- JoblrBackend/JoblrBackend/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class JoblrBackendConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "JoblrBackend"
-
-    def ready(self):
-        import JoblrBackend.signals  # Import the signals to register them
--- JoblrBackend/JoblrBackend/models.py
+++ JoblrBackend/JoblrBackend/models.py
@@ -5,29 +5,21 @@ class User(AbstractBaseUser):
     username = models.CharField(max_length=255, unique=True)
     email = models.EmailField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)  # Add this field
+    is_active = models.BooleanField(default=True)  # Indicates if the user is active
     last_login = models.DateTimeField(null=True, blank=True)
+
+    # User-specific additional data
+    education = models.CharField(max_length=255, default="", blank=True)  # What the user is studying
+    currentlyEmployed = models.BooleanField(default=False)  # Employment status
+    current_organization = models.CharField(max_length=255, default="", blank=True)  # Current workplace
+    current_roll = models.CharField(max_length=255, default="", blank=True)  # Current position/title
+    city = models.CharField(max_length=255, default="", blank=True)  # City of residence
+    country = models.CharField(max_length=255, default="", blank=True)  # Country of residence
+    githubLink = models.URLField(max_length=255, default="", blank=True)  # GitHub profile link
+    linkedinLink = models.URLField(max_length=255, default="", blank=True)  # LinkedIn profile link
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
 
     def __str__(self):
         return self.username
-    
-
-class UserData(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    # MANDATORY FOR THE USER
-    education = models.CharField(max_length=255, unique=False, default="") # Should be whatever the user is studying i.e. B. Computer Science
-    currentlyEmployed = models.BooleanField(default=False) # Currently employed
-    current_organization = models.CharField(max_length=255, unique=False, default="") # This would be the organization the user is working at i.e. PSPC
-    current_roll = models.CharField(max_length=255, unique=False, default="") # This would be his current position/title
-    city = models.CharField(max_length=255, default= "") # User City
-    country = models.CharField(max_length=255, default= "") # User Country
-    
-    # OTHER USER DATA
-    githubLink = models.CharField(max_length=255, default= "") # user github link
-    linkedinLink = models.CharField(max_length=255, default= "") # users linkedin link
-    
-
