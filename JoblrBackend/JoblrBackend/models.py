30c140968fe27efd20e69590888501b6a7946f1e
saidel04 <saidelakad@cmail.carleton.ca>
2024-12-10 08:07:34 -0500

    bug fix

--- JoblrBackend/JoblrBackend/models.py
+++ JoblrBackend/JoblrBackend/models.py
@@ -1,5 +1,40 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        """
+        Create and return a regular user with the given username and email.
+        """
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)  # Hash the password
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        """
+        Create and return a superuser with the given username and email.
+        """
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
+
+    def get_by_natural_key(self, username):
+        """
+        Retrieve a user by their natural key (username).
+        """
+        return self.get(username=username)
+
 
 class User(AbstractBaseUser):
     username = models.CharField(max_length=255, unique=True)
@@ -12,14 +47,16 @@ class User(AbstractBaseUser):
     firstLogin = models.BooleanField(default=True)
 
     # User-specific additional data
-    education = models.CharField(max_length=255, default="", blank=True)  # What the user is studying
-    currentlyEmployed = models.BooleanField(default=False)  # Employment status
-    current_organization = models.CharField(max_length=255, default="", blank=True)  # Current workplace
-    current_roll = models.CharField(max_length=255, default="", blank=True)  # Current position/title
-    city = models.CharField(max_length=255, default="", blank=True)  # City of residence
-    country = models.CharField(max_length=255, default="", blank=True)  # Country of residence
-    githubLink = models.URLField(max_length=255, default="", blank=True)  # GitHub profile link
-    linkedinLink = models.URLField(max_length=255, default="", blank=True)  # LinkedIn profile link
+    education = models.CharField(max_length=255, default="", blank=True)
+    currentlyEmployed = models.BooleanField(default=False)
+    current_organization = models.CharField(max_length=255, default="", blank=True)
+    current_roll = models.CharField(max_length=255, default="", blank=True)
+    city = models.CharField(max_length=255, default="", blank=True)
+    country = models.CharField(max_length=255, default="", blank=True)
+    githubLink = models.URLField(max_length=255, default="", blank=True)
+    linkedinLink = models.URLField(max_length=255, default="", blank=True)
+
+    objects = CustomUserManager()  # Use the custom manager
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['email']
